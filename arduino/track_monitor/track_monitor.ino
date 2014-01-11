78d291947eb4021d96e9430ed8b71f9e6e68fead
Kirk Thompson <kwt104@gmail.com>
2014-01-11 16:23:04 -0500

    add possible track 2

--- arduino/track_monitor/track_monitor.ino
+++ arduino/track_monitor/track_monitor.ino
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
-#define ECHO_PIN_1 7
-#define TRIG_PIN_1 8
-#define LED_PIN 13
+#define TRIG_PIN_1 2
+#define ECHO_PIN_1 3
+#define TRIG_PIN_2 4
+#define ECHO_PIN_2 5
+
 
 long getDurationFromSensor(int trigPin, int echoPin);
 
@@ -11,18 +13,20 @@ void setup() {
   Serial.begin(9600);
   pinMode(TRIG_PIN_1, OUTPUT);
   pinMode(ECHO_PIN_1, INPUT);
-  pinMode(LED_PIN, OUTPUT);
+  pinMode(TRIG_PIN_2, OUTPUT);
+  pinMode(ECHO_PIN_2, INPUT);  
 }
 
 
 void loop() {
-  static long durationTrack1;
+  static long durationTrack1, durationTrack2;
   static char serialMessage[50];
 
-  // TODO: expand this to add a second track.
   durationTrack1 = getDurationFromSensor(TRIG_PIN_1, ECHO_PIN_1);
+  //durationTrack2 = getDurationFromSensor(TRIG_PIN_1, ECHO_PIN_1);
 
   sprintf(serialMessage, "[{\"track\":1,\"value\":%i}]", durationTrack1);
+  //sprintf(serialMessage, "[{\"track\":2,\"value\":%i}]", durationTrack2);
   Serial.println(serialMessage);
 
   delay(30);
